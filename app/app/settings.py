9aa050f921126671afabbf1d3c975a236a8cb62d
Alexander Iordanou <iordanoualexander@gmail.com>
2022-10-03 21:19:42 +0300

    registers CustomUserModel and makes it the default AUTH_USER_MODEL

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+from core.models import CustomUserModel
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'CustomUserModel'
