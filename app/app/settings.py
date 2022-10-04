1cd4d7462e7b8f98c5a8c58745c4ca3e1cc16fd8
Alexander Iordanou <iordanoualexander@gmail.com>
2022-10-04 21:02:40 +0300

    fixes AUTH_USER_MODEL

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import environ
-from core.models import CustomUserModel
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,4 +129,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'CustomUserModel'
+AUTH_USER_MODEL = 'core.CustomUserModel'
