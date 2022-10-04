fbb85d56f0e31ea93f78f70102b4fa1d61368c51
Alexander Iordanou <iordanoualexander@gmail.com>
2022-10-04 21:00:37 +0300

    adds CustormUserManager to models.py

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,4 @@
-from email.policy import default
 from enum import unique
-from unittest.util import _MAX_LENGTH
 from django.db import models
 
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
@@ -8,11 +6,42 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 # Create your models here.
 
+class CustomUserManager(BaseUserManager):
+    """Custom manager for the custom user model"""
+
+    def create_user(self, email, name, password=None):
+        """Method to create a regular user"""
+
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            name = name,
+        )
+
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Method to create a superuser"""
+
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
 class CustomUserModel(AbstractBaseUser, PermissionsMixin):
+    """Custom User model"""
+
     email = models.EmailField(unique = True)
     name = models.CharField(max_length = 20)
     is_active = models.BooleanField(default = True)
     is_staff = models.BooleanField(default = False)
+    is_superuser = models.BooleanField(default = False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
