3eb915d5f6da154063cf16678171055ba1464ea8
Alexander Iordanou <iordanoualexander@gmail.com>
2022-10-04 21:02:01 +0300

    adds CustomUserModel test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_success(self):
         """Test if the user instance creation is succesfull"""
-        
+
         email = 'test@example.com'
         name = 'test name'
         password = 'testpassword'
@@ -19,3 +19,18 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertEqual(user.name, name)
         self.assertTrue(user.check_password(password))
+
+
+    def test_create_superuser_with_email_success(self):
+        """Test if the superuser instance creation is succesfull"""
+
+        email = 'test2@example.com'
+        name = 'test name 2'
+        password = 'testpassword2'
+
+        user = get_user_model().objects.create_superuser(email = email,
+                                                        name = name,
+                                                        password = password,)
+
+        self.assertEqual(user.is_staff, True)
+        self.assertEqual(user.is_superuser, True)
