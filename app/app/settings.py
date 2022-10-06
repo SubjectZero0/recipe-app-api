8527d119c59ef748afa5c5f3829925dbc6852460
Alexander Iordanou <iordanoualexander@gmail.com>
2022-10-06 20:18:42 +0300

    creates and installs User django app

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'User.apps.UserConfig',
     'rest_framework',
     'drf_spectacular',
 ]
