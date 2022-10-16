18a2c5fd6617d80440f774c59c8cb81d1c142087
Alexander Iordanou <iordanoualexander@gmail.com>
2022-10-16 21:22:10 +0300

    registers Tag model in admin.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CustomUserModel, Recipe
+from .models import CustomUserModel, Recipe, Tag
 # Register your models here.
 
 admin.site.register(CustomUserModel)
 admin.site.register(Recipe)
+admin.site.register(Tag)
