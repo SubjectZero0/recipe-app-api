df5811d6b436b39e54dc3cd3f308c3484222c888
Alexander Iordanou <iordanoualexander@gmail.com>
2022-10-20 21:44:02 +0300

    creates Ingredient serializer and nests it in Recipe serializer

--- app/Recipe/serializers.py
+++ app/Recipe/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
+from rest_framework import serializers
 
 
 
@@ -15,14 +16,33 @@ class RecipeTagSerializer(ModelSerializer):
         fields = [ 'id', 'tag_name']
         read_only_fields =['id']
 
+
+class RecipeIngredientSerializer(ModelSerializer):
+    """
+    Serializer for user's recipes' ingresients.
+    Ingredients can be created through creating recipes
+    with ingredients, or through the Ingredient API.
+    """
+    quantity_used = serializers.CharField(max_length = 100)
+
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'ingredient_name', 'quantity_used']
+        read_only_fields = ['id']
+
+
+###########################################################################################
+
+
 class RecipeSerializer(ModelSerializer):
     """
     Serializer for Recipes.
-    Automatic Tag creation and update in Tag API,
+    Automatic Tag and Ingredient creation and update in Tag/Ingredient API,
     through many-to-many relationship.
     """
 
     tags = RecipeTagSerializer(many=True, required = False) #use of nested serializer
+    ingredients = RecipeIngredientSerializer(many=True, required=False) #use of nested serializer
 
     class Meta:
         model = Recipe
@@ -35,7 +55,7 @@ class RecipeSerializer(ModelSerializer):
     def _get_or_create_tags(self, tags, recipe):
         """
         Helper Function.
-        Get or create tags, if non exist.
+        Get, or create tags, if non exist.
         ADDS the new tags in the recipe.
         """
         authenticated_user = self.context['request'].user #gets the authenticated user that makes the post request.
@@ -49,19 +69,33 @@ class RecipeSerializer(ModelSerializer):
 
             recipe.tags.add(tag_obj) #adds the tag object in the recipe, instead of a direct post request, which is not allowed.
 
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """
+        Helper function.
+        Get, or create ingredients, if non exist.
+        ADDS the new tags in the recipe.
+        """
+        authenticated_user = self.context['request'].user
+
+        for ingredient in ingredients:
+            ingr_obj, created = Ingredient.objects.get_or_create(user=authenticated_user, **ingredient)
+            recipe.ingredients.add(ingr_obj)
+
 
     def create(self, validated_data):
         """
         Custom Method for Creating a recipe, with
-        nested serializer - RecipeTagSerializer,
-        since direct creation with nested serializers is not allowed.
-        If a Tag doesnt exist in the Tag table, it will be created,
-        and will be available in the Tag API.
+        nested serializers, since direct creation
+        with nested serializers/manytomany is not allowed.
+        If a Tag/Ingredient doesnt exist in the appropriate table,
+        it will be created, and will be available in the Tag API.
         """
 
         tags = validated_data.pop('tags', []) #if any tags are passed into the serializer, removes them and places them in tags variable. Else its an empty list.
+        ingredients = validated_data.pop('ingredients', []) #if any ingredients are passed into the serializer, removes them and places them in tags variable. Else its an empty list.
         recipe = Recipe.objects.create(**validated_data) #creates a recipe with the rest of the validated data.
         self._get_or_create_tags(tags, recipe)#calls helper function to get or create tags and ADD them to recipe.
+        self._get_or_create_ingredients(ingredients, recipe) #calls helper function to get or create ingredients and ADD them to recipe.
 
         return recipe
 
@@ -70,6 +104,7 @@ class RecipeSerializer(ModelSerializer):
         As with creation, custom method for updating recipe
         """
         tags = validated_data.pop('tags', None)#if any tags are passed into the serializer, removes them and places them in tags variable.
+        ingredients = validated_data.pop('ingredients', None)
 
         if tags is not None:
             """
@@ -78,6 +113,14 @@ class RecipeSerializer(ModelSerializer):
             """
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+
+        if ingredients is not None:
+            """
+            if ingredients exist, clears them.
+            Calls helper function to create ingredients and ADD them to instance(recipe)
+            """
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             """
